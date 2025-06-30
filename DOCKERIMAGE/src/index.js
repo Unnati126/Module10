e539e2d085d294b6f55047647384522ca31ab540
Unnati Gadhiya <unnatigadhiya0206@gmail.com>
2025-06-30 20:23:32 +1000

    init commit

---
+++ DOCKERIMAGE/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+// parse requests of content-type - application / json;
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my docker image lab." });
+});
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
